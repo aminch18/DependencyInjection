600e7d5ad3f6e351197b9c160d5c65756bb327ff
Amin Chouaibi El Azaar <95026434@lidl.de>
2020-02-17 15:12:42 +0100

    Adding classes and implemented without DI

--- DependencyInjection/Program.cs
+++ DependencyInjection/Program.cs
@@ -6,7 +6,54 @@ namespace DependencyInjection
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var gun = new Gun();
+            var shotGun = new ShotGun();
+            var machineGun = new MachineGun();
+
+            gun.Shoot();
+            shotGun.Shoot();
+            machineGun.Shoot();
         }
     }
+
+    #region Classes!
+    public class Gun
+    {
+        public Gun()
+        {
+
+        }
+
+        public void Shoot()
+        {
+            Console.WriteLine("Bang! Bang!");
+        }
+    }
+
+    public class MachineGun
+    {
+        public MachineGun()
+        {
+
+        }
+
+        public void Shoot()
+        {
+            Console.WriteLine("Ta-ta-ta-ta-ta!");
+        }
+    }
+
+    class ShotGun
+    {
+        public ShotGun()
+        {
+        }
+
+        public void Shoot()
+        {
+            Console.WriteLine("Puuuuuuum!");
+        }
+    }
+
+    #endregion
 }
